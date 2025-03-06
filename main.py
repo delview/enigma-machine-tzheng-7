69afc683412f6087bb0b01dff7bee371f86972aa
tzheng-7 <tiantianz258@deltalearns.ca>
2025-03-06 21:15:30 +0000

    Added code that greets user & asks for basic inputs
    - Added function (greet()) that greets user & asks for string & if they want to encrypt/decrypt
    - Added variable (d_or_e) that stores user's choice of encryption/decryption
    - Added variable (og_string) that stores string user wants to encrypt/decrypt
    - Added if statements that call functions accordingly to user's choice of encryption/decryption
    - Added code that calls greet()

--- main.py
+++ main.py
@@ -15,13 +15,28 @@
 # Choose cipher to use for program
 
 # Ask user whether they want to decrypt/encrypt a message
+def greet():
+    d_or_e = input("\nHello! Hope you're having a great day <3\n\nWould you like to [e]ncrypt or [d]ecrypt?\n\n> ")
+    og_string = input(f"\nThanks! Now please enter a string.\n\n> ")
+    if d_or_e == 'e':
+        encrypt()
+    if d_or_e == 'd':
+        decrypt()
 
 # Accept string that program will either encrypt/decrypt
 
 # Call function to encrypt/decrypt
 
 # Create 2 functions that'll accept parameters in the form of list/dictionary that will contain string to be encrypted/decrypted. One will encrypt, other will decrypt using chosen cipher technique. This should involve loops & if statements
+def encrypt():
+    print("waa")
+
+def decrypt():
+    print("uieafife")
 
 # Present final encrypted/decrypted string to user
 
 # User must be able to copy output & run through program again to perfectly encrypt/decrypt multiple times without mistakes - test this
+
+# Call functions
+greet()
