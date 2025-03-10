347ebdc08b8b9d3e45062d51aa56a17514d7a284
tzheng-7 <tiantianz258@deltalearns.ca>
2025-03-10 16:22:12 +0000

    Function coding since they're not working properly
    - Fixed encrypt(og_string)
    - Changed og_string from list to string

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Create morse code list
 morse_code = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
 
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-char_to_index = {char: idx for idx, char in enumerate(alphabet)}
+char_to_morse = {char: morse for char, morse in zip(alphabet, morse_code)}
+
+morse_to_char = {morse: char for morse, char in zip(morse_code, alphabet)}
 
 # Create empty list to store og string
 og_string = []
@@ -11,11 +13,11 @@ og_string = []
 # Create empty list to store encrypted/decrypted string
 changed_string = []
 
-# 8 commits
+# - 8 commits
 
-# Use .strip()
+# - Use .strip()
 
-# Use functions whenever you can (eg. a function to encrypt, a function to decrypt, a function to format the string)
+# - Use functions whenever you can (eg. a function to encrypt, a function to decrypt, a function to format the string)
 
 # Functions can either output directly or return the string to where it was called (eg. your formatting function could call for decrypting and then it gets returned to the formatting function so that it can be formatted nicely)
 
@@ -26,40 +28,39 @@ def greet():
     '''
     Greets user, asks user for choice of encryption/decryption, & asks user for string they want to encrypt/decrypt
     '''
-    d_or_e = input("\nHello! Hope you're having a great day <3\n\nWould you like to [e]ncrypt or [d]ecrypt?\n\n> ") # Greets user & asks user for choice of encryption/decryption
-    og_string.append(input(f"\nThanks! Now please enter a string.\n\n> ").upper()) # Asks user for string they want to encrypt/decrypt
+    d_or_e = input("\nWould you like to [e]ncrypt or [d]ecrypt?\n\n> ") # Greets user & asks user for choice of encryption/decryption
     if d_or_e == 'e': # If user's choice is to encrypt
+        og_string = input(f"\nNow, please enter the message you would like to encrypt!\n\n> ").upper().strip() # Asks user for string they want to encrypt
         encrypt(og_string) # Call function to encrypt
     elif d_or_e == 'd': # If user's choice is to decrypt
-        decrypt() # Call function to decrypt
-    else:
-        print("\nInvalid input...")
-        greet()
+        og_string = input(f"\nNow, please enter the message you would like to decrypt!\n\n> ").strip().split() # Asks user for string they want to decrypt
+        decrypt(og_string) # Call function to decrypt
+    else: # If user's input's invalid
+        print("\nOops, invalid input :c") # Give feedback
+        greet() # Loop till user gives valid input
 
 # Create 2 functions that'll accept parameters in the form of list/dictionary that will contain string to be encrypted/decrypted. One will encrypt, other will decrypt using chosen cipher technique. This should involve loops & if statements
 def encrypt(og_string):
     '''
-    Encrypts user's string
+    Encrypts user's string & outputs it
     '''
-    for char in og_string[0]:
-        if char in char_to_index:
-            index = char_to_index[char]  # Get index of character
-            changed_string.append(morse_code[index])
-            print(encrypt(og_string))
-        else:
-            changed_string.append('')
-            print("\n")
-        print(" ".join(changed_string))
+    for char in og_string:
+        if char in char_to_morse: # If char is letter
+            changed_string.append(char_to_morse[char]) # Change to morse code according to idx
+        else: # If char is punctuation
+            changed_string.append('') # Ignore it
 
-def decrypt():
+    print("\nEncrypted message:") # Display encrypted msg
+    print(" ".join(changed_string)) # Present final encrypted/decrypted string to user
+    print("\n")
+
+def decrypt(og_string):
     '''
     Decrypts user's string
     '''
-    print("uieafife")
 
-# Present final encrypted/decrypted string to user
-
-# User must be able to copy output & run through program again to perfectly encrypt/decrypt multiple times without mistakes - test this
+# - User must be able to copy output & run through program again to perfectly encrypt/decrypt multiple times without mistakes - test this
 
 # Run program
+print("\nHello! Hope you're having a great day <3") # Greet user before calling functions so it's not looped if input is invalid
 greet()
