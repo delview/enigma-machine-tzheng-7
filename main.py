2147174269909d97ae55f89c0aef6ce764f065ba
tzheng-7 <tiantianz258@deltalearns.ca>
2025-03-11 18:23:33 +0000

    More work with encrypting and decrypting functions - getting ready to add formatting function
    - Moved changed_string = [] lines into functions so it's defined
    - Added code in decrypt(og_string) and encrypt(og_string) that calls for format(changed_string) (I DIDN'T ADD IT YET BUT I WILL !!)

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 # Create morse code list
-morse_code = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+morse_code = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '/']
 
-alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
+alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ']
 
 char_to_morse = {char: morse for char, morse in zip(alphabet, morse_code)}
 
 morse_to_char = {morse: char for morse, char in zip(morse_code, alphabet)}
 
-# Create empty list to store og string
-og_string = []
-
 # Create empty list to store encrypted/decrypted string
-changed_string = []
 
 # - 8 commits
 
@@ -21,7 +17,7 @@ changed_string = []
 
 # Functions can either output directly or return the string to where it was called (eg. your formatting function could call for decrypting and then it gets returned to the formatting function so that it can be formatted nicely)
 
-# Use variables, lists or dictionaries or tuples, string formatting, loops, if statements, functions
+# Use string formatting
 
 # Ask user whether they want to decrypt/encrypt a message
 def greet():
@@ -39,32 +35,40 @@ def greet():
         print("\nOops, invalid input :c") # Give feedback
         greet() # Loop till user gives valid input
 
+# Create function that properly formats output
+def format(changed_string):
+    '''
+    Formats changed string properly
+    '''
+
 # Create 2 functions that'll accept parameters in the form of list/dictionary that will contain string to be encrypted/decrypted. One will encrypt, other will decrypt using chosen cipher technique. This should involve loops & if statements
 def encrypt(og_string):
     '''
     Encrypts user's string & outputs it
     '''
+    changed_string = []
     for char in og_string:
         if char in char_to_morse: # If char is letter
             changed_string.append(char_to_morse[char]) # Change to morse code according to idx
         else: # If char is punctuation
             changed_string.append('') # Ignore it
-    print("\nEncrypted message:") # Display encrypted msg
-    print(" ".join(changed_string)) # Present final encrypted/decrypted string to user
-    print("\n")
+
+    final_output = " ".join(changed_string)
+    print(f"\nEncrypted message: {final_output}\n") # Display encrypted msg
 
 def decrypt(og_string):
     '''
     Decrypts user's string
     '''
+    changed_string = []
     for morse in og_string:
         if morse in morse_to_char: # If char is morse code
             changed_string.append(morse_to_char[morse]) # Change to alphabet according to idx
         else: # If char is punctuation
             changed_string.append('') # Ignore it
-    print("\nEncrypted message:") # Display encrypted msg
-    print(" ".join(changed_string)) # Present final encrypted/decrypted string to user
-    print("\n")
+
+    final_output = format("".join(changed_string))
+    print(f"\nEncrypted message: {final_output}\n") # Display encrypted msg
 
 # - User must be able to copy output & run through program again to perfectly encrypt/decrypt multiple times without mistakes - test this
 
